af2bcebd90b2bda8527d0444bf13c6cb3fa1991d
Azhar abbas <93605364+azhar12102@users.noreply.github.com>
2024-05-25 23:22:53 +0500

    Add files via upload

---
+++ Question2.js
@@ -0,0 +1,2 @@
+var name1 = "Eric";
+console.log("Hello", name1, "would you like to learn some python today?");
---
+++ Question2.ts
@@ -0,0 +1,2 @@
+let personname : string = "Eric";
+console.log(`Hello ${personname} would you like to learn some python today?`);
